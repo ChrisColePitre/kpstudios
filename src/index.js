e6697c3b896f2af5f79ce3fc2af14369a5dd0be7
chriscolepitre <chriscolepitre@gmail.com>
2021-11-26 23:39:56 -0500

    lowercase changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Second from './Second';
-import Third from './Third';
+// import App from './app';
+import Third from './third';
 import reportWebVitals from './reportWebVitals';
 
 //could I write as many ReactDOM.render functions calls as I want? Could I plug in props from an HTML element? 
@@ -15,22 +14,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Second />
-//   </React.StrictMode>,
-//   document.getElementById('wat')
-// );
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Third />
-//   </React.StrictMode>,
-//   document.getElementById('wut')
-// );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
