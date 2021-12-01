9d43d1b806f14d05690e3399cca264748f3fe017
chriscolepitre <chriscolepitre@gmail.com>
2021-12-01 09:24:25 -0500

    makin more fixes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './app';
-import Third from './third';
-import reportWebVitals from './reportWebVitals';
+import Home from './components/home'
+
 
 //could I write as many ReactDOM.render functions calls as I want? Could I plug in props from an HTML element? 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Third />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 
-reportWebVitals();
+
