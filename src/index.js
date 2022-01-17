f64a2f84c9ff36d740beeefd5cb074a7d858dc8d
chriscolepitre <chriscolepitre@gmail.com>
2022-01-17 15:24:01 -0500

    smol fix for netlify

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './app';
-import Home from './components/home'
+// import Home from './components/home'
 import Card from './components/universalcard'
 
 
